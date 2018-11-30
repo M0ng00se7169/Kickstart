7775e30cb6ded0dfda2fc98f9d71414501a88c8f
Nikita Pyanykh <nickkomarowski@gmail.com>
2018-11-30 15:30:18 +0200

    7. Automatic Routing Added

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -7,11 +7,14 @@ import web3 from '../../etherium/web3'
 class CampaignNew extends Component {
     state = {
         minimumContribution: '',
-        errorMessage: ''
+        errorMessage: '',
+        loading: false
     }
 
     onSubmit = async (event) => {
         event.preventDefault();
+
+        this.setState({ loading: true, errorMessage: '' })
         
         try{
             const accounts = await web3.eth.getAccounts()
@@ -24,6 +27,7 @@ class CampaignNew extends Component {
             this.setState({ errorMessage: err.message })
         }
         
+        this.setState({ loading: false })
     }
     
     render() {
@@ -42,7 +46,7 @@ class CampaignNew extends Component {
                         />
                     </Form.Field>
                     <Message error header="Oops!" content={this.state.errorMessage} />
-                    <Button primary>Create!</Button>
+                    <Button loading={this.state.loading} primary>Create!</Button>
                 </Form>
             </Layout>
         )
