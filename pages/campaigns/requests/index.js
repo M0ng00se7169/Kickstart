54f535206c9c8b08dc8584bc0d6d29dc8460a9f8
Nikita Pyanykh <nickkomarowski@gmail.com>
2018-12-02 21:20:10 +0200

    3. Added Requests Feature

---
+++ pages/campaigns/requests/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class RequestIndex extends Component {
+    render() {
+        return (
+            <h3>Requests List</h3>
+        )
+    }
+}
+
+export default RequestIndex
