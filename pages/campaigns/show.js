018c5e6350ee35cba0e902c809cf39968b6b387e
Nikita Pyanykh <nickkomarowski@gmail.com>
2018-12-02 12:40:18 +0200

    2. Added Summary Translation Layer

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react'
+import Layout from '../../components/Layout'
+import Campaign from '../../etherium/campaign'
 
 class CampaignShow extends Component {
+    static async getInitialProps(props) {
+        const campaign = Campaign(props.query.address) // Actual Address of the Campaign
+
+        const summary = await campaign.methods.getSummary().call()
+
+        return { summary: summary }
+    }
+    
     render() {
-        return <h3>Campaign Show</h3>
+        return (
+            <Layout>
+                <h3>Campaign Show</h3>
+            </Layout>
+        )
     }
 }
 
