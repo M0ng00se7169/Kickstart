2097feec2307d5b51cb0e219d6bb0491e4b69356
Nikita Pyanykh <nickkomarowski@gmail.com>
2018-12-03 22:09:06 +0200

    2. Prepare for deploy

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const { createServer } = require('http')
 const next = require('next')
+const { parse } = require('url')
 
 const app = next({
     dev: process.env.NODE_ENV !== 'production'
 })
 
 const routes = require('./routes')
-const handler = routes.getRequestHandler(app)
-
+//const handler = routes.getRequestHandler(app)
+const handler = app.getRequestHandler()
 app.prepare().then(() => {
-    createServer(handler).listen(3000, (err) => {
+    createServer((req, res) => {
+        const parsedUrl = parse(req.url, true)
+        handler(req, res, parsedUrl)
+    }).listen(3000, err => {
         if (err) throw err
-        console.log('Ready on localhost:3000...')
+        console.log('Ready on http://localhost:3000')
     })
 })
+
+// app.prepare().then(() => {
+//     createServer(handler).listen(3000, (err) => {
+//         if (err) throw err
+//         console.log('Ready on localhost:3000...')
+//     })
+// })
