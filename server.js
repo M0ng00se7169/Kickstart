9f975cdc96674253f74c3071fd550f7caf3ee129
Nikita Pyanykh <nickkomarowski@gmail.com>
2018-12-03 22:13:11 +0200

    3. deploy

--- server.js
+++ server.js
@@ -7,21 +7,21 @@ const app = next({
 })
 
 const routes = require('./routes')
-//const handler = routes.getRequestHandler(app)
-const handler = app.getRequestHandler()
-app.prepare().then(() => {
-    createServer((req, res) => {
-        const parsedUrl = parse(req.url, true)
-        handler(req, res, parsedUrl)
-    }).listen(3000, err => {
-        if (err) throw err
-        console.log('Ready on http://localhost:3000')
-    })
-})
-
+const handler = routes.getRequestHandler(app)
+// const handler = app.getRequestHandler()
 // app.prepare().then(() => {
-//     createServer(handler).listen(3000, (err) => {
+//     createServer((req, res) => {
+//         const parsedUrl = parse(req.url, true)
+//         handler(req, res, parsedUrl)
+//     }).listen(3000, err => {
 //         if (err) throw err
-//         console.log('Ready on localhost:3000...')
+//         console.log('Ready on http://localhost:3000')
 //     })
 // })
+
+app.prepare().then(() => {
+    createServer(handler).listen(3000, (err) => {
+        if (err) throw err
+        console.log('Ready on localhost:3000...')
+    })
+})
